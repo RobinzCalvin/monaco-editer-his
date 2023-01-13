343b30d73513f4b7d634a17945c01dc2da886455
Robinz Calvin <robinzcalvin@gmail.com>
2023-01-13 06:44:50 -0500

    fiveupdate

--- src/component/fileOpen.js
+++ src/component/fileOpen.js
@@ -9,35 +9,32 @@ const FileOpen = (props) => {
         inputRef.current.click();
     };
     const onChange = e => {
-      let file = e.target.files;
-      fileReader = new FileReader();
-      fileReader.onloadend = handleFileRead;
-      fileReader.readAsText(file[0]);
-      setFileUrl(file[0]);
-      // console.log('sdfffffffffffffffffffffff',file);
-     
+        let file = e.target.files;
+        fileReader = new FileReader();
+        fileReader.onloadend = handleFileRead;
+        fileReader.readAsText(file[0]);
+        setFileUrl(file[0]);
     };
     const handleFileRead = (e) => {
-      let content = fileReader.result;
-      setTempgivestring([{
-        'string': fileReader.onload,
-        'date': '',
-        'offSetNum': 0,
-      }
-      ])
-      setFileContent(content);
-      console.log('sdfffffffffffffffffffffff',content);
+        let content = fileReader.result;
+        setTempgivestring([{
+            'string': fileReader.onload,
+            'date': '',
+            'offSetNum': 0,
+        }
+        ])
+        setFileContent(content);
     };
     return (
-      <div className='ml-5'>
-      <input className=' fileurl '
-          style={{display: 'none'}}
-          ref={inputRef}
-          type="file"
-          onChange={onChange}
-      />
-        <button onClick={handleClick} className=" rounded text-white text-xl font-medium bg-[#0092cc] hover:bg-[#004b69] pr-2 pl-2">File Open</button>
-      </div>
+        <div className='ml-5'>
+            <input className=' fileurl '
+                style={{display: 'none'}}
+                ref={inputRef}
+                type="file"
+                onChange={onChange}
+            />
+            <button onClick={handleClick} className=" rounded text-white text-xl font-medium bg-[#0092cc] hover:bg-[#004b69] pr-2 pt-1 pb-1 pl-2">File Open</button>
+        </div>
     );
 };
 export default FileOpen;    
--- src/component/myeditor.js
+++ src/component/myeditor.js
@@ -1,21 +1,20 @@
-import Editor, { monaco,useMonaco } from "@monaco-editor/react";
-// import files from "./files";
-import React, { useState,useCallback, useEffect, useRef } from "react";
+
+import Editor from "@monaco-editor/react";
+import React, { useCallback, useState, useEffect, useRef } from "react";
 import { Config } from "../config/config";
 import FileOpen from "./fileOpen";
+import "./index.css";
 export function Myeditor() {
-    async function handleEditorDidMount(monacoEditor, monaco) {
-        setVars({ monacoEditor, monaco });
-      }
     const [fileUrl, setFileUrl] = useState([]);
     const [displayList, setDisplayList] = useState([]);
     const [fileContent, setFileContent] = useState('');
     const [changeString, setChangeString] = useState('');
     const [offSetNum, setOffSetNum] = useState(0);
-    const [vars, setVars] = useState({});
-    const [ids, setIds] = useState([]);
-    const { monacoEditor, monaco } = vars;
     const [changeCounts, setChangeCounts] = useState(0);
+    const [startLines, setStartLines] = useState(1);
+    const [startColumns, setStartColumns] = useState(1);
+    const [endLines, setEndLines] = useState(1);
+    const [endColumns, setEndColumns] = useState(1);
     const [giveListLog, setGiveListLog] = useState([
         {
         'string': '',
@@ -30,54 +29,33 @@ export function Myeditor() {
         'offSetNum': 0,
         }
     ]);
-    const handleChangeIds = useCallback(() => {
-        if (!monacoEditor || !monaco) {
-          return;
-        }
-        setIds((ids) => {
-          const newIds = monacoEditor.deltaDecorations(ids, [
-            {
-              range: new monaco.Range(1, 1, 1, 5),
-              options: { inlineClassName: "base.case.lol" }
-            }
-          ]);
-          return newIds;
-        });
-        
-      }, [monacoEditor, monaco]);
+    const [vars, setVars] = useState({});
+    const { monacoEditor, monaco } = vars;
     let config = Config;
-    let temp = '';
-    const [ count, setCount ] = useState(0);
+    const [count, setCount ] = useState(1);
     const timerRef = useRef();
+    async function editorDidMount(monacoEditor, monaco) {
+        setVars({ monacoEditor, monaco });
+    }
+
     useEffect(() => {
         const displayList = giveListLog.map(item => Object.assign(item, {show: false}))
         setDisplayList(displayList);
     }, [changeString])
 
-    
-    // This executes once upon mounting
-    // i.e. equivalent to componentDidMount() in a class component
     useEffect(() => {
-        // Register a function to increment the count every second
-        // and capture the timer handle so we can cancel it later.
         timerRef.current = setInterval(() => {
             setCount(c => c + 1);
         }, 1000);
-        // Clean up when the component is unmounting
-        // i.e. equivalent to componentWillUnmount() in a class
-        
         return () => {
             clearInterval(timerRef.current);
         };
     }, [ timerRef, setCount ]);
-    // 
+
     useEffect(()=>{
         let tempGive = [];
-        // setTimeout(() => {
-            
-        // }, 1000);
-        if (count % 5 == 0) {
-            if(tempgivestring[0].string !== changeString){
+        if(tempgivestring[0].string !== changeString){
+            if (count % 10 === 0 ) {
                 tempGive = [
                     {
                         'date': new Date().toLocaleString(),
@@ -85,60 +63,82 @@ export function Myeditor() {
                         'offSetNum': offSetNum,
                     },
                 ];
-                setGiveListLog([...giveListLog, ...tempGive]);
+                console.log('tempGive', tempGive);
                 setTempgivestring(tempGive);
-                console.log('givestringlgo.string', tempgivestring[0].string);
-                console.log('changestring', changeString);
+                setGiveListLog([...giveListLog, ...tempGive]);
+                const obj = [...giveListLog, ...tempGive];
+                const blob = new Blob([JSON.stringify(obj, null, 2)], {type : 'application/json'});
+                saveFile(blob, fileUrl);
             }  
-            console.log('givelistlog', giveListLog);
-        }  
+        }
     }, [count])
+
+    useEffect(() => {
+        if (!monacoEditor || !monaco) {
+          return;
+        }
+        const qwe = monacoEditor.deltaDecorations(
+          [], [{ range: new monaco.Range(startLines, startColumns, endLines, endColumns), options: { inlineClassName: "base.case.rofl" }}]  
+        );
+        return () => monacoEditor.deltaDecorations(qwe, []);
+    }, [changeString]);   
+
     const handleClickInfo = (selectedIndex) => {
-        console.log('givelistlog');
         const newDisplayList = displayList.map((item, index) => {
             if(index === selectedIndex) item.show = !(item.show);
             return item;
         });
         setDisplayList(newDisplayList);
     }
-    const handleEditorChange = (value, event) => {
-        // event.getValue();
+
+    const handleEditorChange = (count, event) => {
         let a=changeCounts;
         setChangeCounts(a++);
         let temp1 = '';
         temp1 = changeString +  event.changes[0].text;
         setChangeString(temp1);
         setOffSetNum(parseInt(event.changes[0].rangeOffset));
-      }
+        setEndLines(event.changes[0].range.startLineNumber);
+        setEndColumns(event.changes[0].range.startColumn);
+        if (count % 5 === 0 && tempgivestring[0].string !== changeString) {
+            setStartLines(event.changes[0].range.startLineNumber);
+            setStartColumns(event.changes[0].range.startColumn);
+        }    
+    }
+
+    const saveFile = async (blob, fileUrl) => {
+        const a = document.createElement('a');
+        a.download = fileUrl.name;
+        a.href = URL.createObjectURL(blob);
+        a.addEventListener('click', (e) => {
+          setTimeout(() => URL.revokeObjectURL(a.href), 30 * 1000);
+        });
+        a.click();
+    }
     return (
         <div className="w-full flex justify-center flex-col mt-5  rounded border-b-1 border-[#451356] ">
             <div className="w-full text-4xl font-bold text-[#451356] h-12 text-center">{config.title}</div>
             <div className="w-full">
-                <FileOpen setFileContent={setFileContent} setFileUrl = {setFileUrl} setTempgivestring = {setTempgivestring}/>
+                <FileOpen setTempgivestring={setTempgivestring} setFileContent={setFileContent} setFileUrl={setFileUrl}/>
             </div>
             <div className="w-full flex flex-row border ">
-                <div className="w-2/3 border-spacing-1 border-[#000000] ">
-                    
-                    <Editor
-                        height="60vh"
-                        path={fileUrl.name}
-                        defaultLanguage={fileUrl.type}
-                        value={fileContent}
-                        onChange={handleEditorChange}
-                    /> 
+                <div className="w-2/3 border-spacing-1 border-[#000000] ">                    
+                <EditorWrapper
+                    path={fileUrl.name} defaultLanguage={fileUrl.type} value={fileContent} handleEditorChange={handleEditorChange} editorDidMount={editorDidMount}/>  
                 </div>
                 <div className="w-1/3  bg-[#D9E8F5]  " >
-                    <div className='flex-col p-2'>
-                        <p className='text-2xl'>Tracked Changes</p>
+                    <div className='flex-col p-5'>
+                        <p className='text-2xl font-bold'>Tracked Changes</p>
                         
-                        <div className=' object-none object-center'>
+                        <div className='border-spacing-1 '>
+                            
                             <div className='h-auto'>
                                 {giveListLog && giveListLog.map((item, index) =>(
-                                    <div key = {index} className='border border-spacing-1 border-b-indigo-400'>
-                                        <p className=' text-xl  '>{index+1}.Tracked Changes</p>
-                                        <p className='text-mm border border-spacing-1 border-b-indigo-200 cursor-pointer' onClick={() => handleClickInfo(index)}>{item.show ? item.string : item.string.slice(0, 5)+'...'}</p>
-                                        <p className="text-sm  ">Tracked Number:{item.offSetNum}</p>
-                                        <p className="text-sm ">Tracked Date:{item.date}</p>
+                                    <div key = {index} className='border rounded-xl shadow-slate-700 border-b-indigo-400'>
+                                        <p className='text-xl font-medium'>Tracked Changes:{changeCounts}</p>
+                                        <p className='text-mm pl-5 hover:text-lime-800 cursor-pointer' onClick={() => handleClickInfo(index)}>{item.show ? item.string : item.string.slice(0, 5)+'...'}</p>
+                                        <p className="text-sm  pl-5">{item.offSetNum}</p>
+                                        <p className="text-sm pl-5">{item.date}</p>
                                     </div>
                                 ))}   
                             </div>
@@ -147,6 +147,17 @@ export function Myeditor() {
                 </div>
             </div>
         </div>
-       
     )
+}
+function EditorWrapper({ theme, path,defaultLanguage,value, language, editorDidMount, handleEditorChange }) {
+    return (
+        <Editor
+            height="80vh"
+            theme={theme}
+            path={path}
+            defaultLanguage={defaultLanguage}
+            value={value}
+            onMount={editorDidMount}
+            onChange={handleEditorChange} /> 
+    );
 }
